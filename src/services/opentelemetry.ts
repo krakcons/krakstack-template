2eccfe62d8cf5423369a0d3e6fcb4de91a53db4a
Billy Hawkes <billyhawkes02@gmail.com>
2026-04-27 19:05:09 -0400

    task: add logs/metrics

--- src/services/opentelemetry.ts
+++ src/services/opentelemetry.ts
@@ -1,6 +1,10 @@
 import { NodeSdk } from "@effect/opentelemetry";
+import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { BatchLogRecordProcessor } from "@opentelemetry/sdk-logs";
 import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { Config, Context, Effect, Layer } from "effect";
 
 const OtelLive = NodeSdk.layer(
@@ -15,6 +19,13 @@ const OtelLive = NodeSdk.layer(
       spanProcessor: new BatchSpanProcessor(
         new OTLPTraceExporter({ url: `${otlpEndpoint}/v1/traces` }),
       ),
+      logRecordProcessor: new BatchLogRecordProcessor(
+        new OTLPLogExporter({ url: `${otlpEndpoint}/v1/logs` }),
+      ),
+      metricReader: new PeriodicExportingMetricReader({
+        exporter: new OTLPMetricExporter({ url: `${otlpEndpoint}/v1/metrics` }),
+        exportIntervalMillis: 10000,
+      }),
     };
   }),
 );
