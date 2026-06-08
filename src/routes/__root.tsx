53853def370e02075b9178aea1dc2b1eb750da6f
Billy Hawkes <billyhawkes02@gmail.com>
2026-06-08 16:56:27 -0400

    task: add analytics build variable

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,6 +4,8 @@ import { m } from "../paraglide/messages.js";
 import { getLocale } from "../paraglide/runtime.js";
 import appCss from "../styles.css?url";
 
+const analyticsWebsiteId = import.meta.env.VITE_ANALYTICS_WEBSITE_ID;
+
 export const Route = createRootRoute({
   head: () => ({
     meta: [
@@ -36,6 +38,15 @@ export const Route = createRootRoute({
         href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
       },
     ],
+    scripts: analyticsWebsiteId
+      ? [
+          {
+            defer: true,
+            src: "https://analytics.krakconsultants.net/script.js",
+            "data-website-id": analyticsWebsiteId,
+          },
+        ]
+      : [],
   }),
   shellComponent: RootDocument,
 });
